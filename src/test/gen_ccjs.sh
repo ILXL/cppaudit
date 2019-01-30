4595a54d1d5c4c2ab4b14d72dfb4c418d7eb99ce
Paul Inventado <pinventado@fullerton.edu>
2019-01-29 19:47:30 -0800

    Use clang++ instead of g++

--- src/test/gen_ccjs.sh
+++ src/test/gen_ccjs.sh
@@ -7,20 +7,20 @@ HEADERS=$4
 ENTRIES="[\n"
 
 ENTRIES=${ENTRIES}"  {\n   \"directory\" : \"$WD\",\n"
-ENTRIES=${ENTRIES}"    \"command\" : \"g++ -std=c++17 $DRIVER -o $1\",\n"
+ENTRIES=${ENTRIES}"    \"command\" : \"clang++ -std=c++17 $DRIVER -o $1\",\n"
 ENTRIES=${ENTRIES}"    \"file\" : \"$WD/$DRIVER\"\n  },\n"
 
 for implem in $IMPLEMS
 do
   ENTRIES=${ENTRIES}"  {\n   \"directory\" : \"$WD\",\n"
-  ENTRIES=${ENTRIES}"    \"command\" : \"g++ -std=c++17 $implem -o $1\",\n"
+  ENTRIES=${ENTRIES}"    \"command\" : \"clang++ -std=c++17 $implem -o $1\",\n"
   ENTRIES=${ENTRIES}"    \"file\" : \"$WD/$implem\"\n  },\n"
 done
 
 for header in $HEADERS
 do
   ENTRIES=${ENTRIES}"  {\n   \"directory\" : \"$WD\",\n"
-  ENTRIES=${ENTRIES}"    \"command\" : \"g++ -std=c++17 $header -o $1\",\n"
+  ENTRIES=${ENTRIES}"    \"command\" : \"clang++ -std=c++17 $header -o $1\",\n"
   ENTRIES=${ENTRIES}"    \"file\" : \"$WD/$header\"\n  },\n"
 done
 ENTRIES=${ENTRIES}"]"
