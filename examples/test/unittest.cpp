c5e583676c1120e5386c069d23a75db0833c68c6
Paul Inventado <pinventado@fullerton.edu>
2019-08-04 06:16:53 +0000

    Separate variables in makefile This will allow us to easily copy and paste changes new files without overwriting current configurations

--- examples/test/unittest.cpp
+++ examples/test/unittest.cpp
@@ -1,23 +1,10 @@
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
-#include <random>
-#include <cmath>
 #include "gtest_ext.h"
-#include "../algebra.hpp"
 
-TEST(CashBack, OutputFormat)
-{
-  ASSERT_DURATION_LE(3, {
-    ASSERT_EXECIO_EQ("restaurant", "", "Restaurant: 27\n");
-  });
-}
-
-TEST(CashBack, FuncTest)
-{
-      ASSERT_EQ(cube(3), 27);
-}
+// Place unit tests here
 
 int main(int argc, char **argv) {
-   testing::InitGoogleTest(&argc, argv);
-   return RUN_ALL_TESTS();
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
