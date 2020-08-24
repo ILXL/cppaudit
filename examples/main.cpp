2dd8e2ded2679b7e9466d79b78eb72ca8953f087
Paul Inventado <pinventado@gmail.com>
2020-08-24 12:44:05 -0700

    Modify sample so that it passes the test

--- examples/main.cpp
+++ examples/main.cpp
@@ -1,9 +1,32 @@
+#include <iomanip>
 #include <iostream>
-#include "algebra.hpp"
-using namespace std;
 
-int main()
-{
-  cout << "Restaurant: " << cube(3) << endl;
+int main() {
+  const double CA_TAX = 0.075;
+  const int TIP_DIVISOR = 100;
+  double meal_cost;
+  double tip_percentage;
+  double taxes;
+  double tip;
+  double total;
+
+  std::cout << "Please input meal cost: ";
+  std::cin >> meal_cost;
+  std::cout << "Please input tip percentage: ";
+  std::cin >> tip_percentage;
+  std::cout << "\n";
+  std::cout << "Restaurant Bill\n";
+  std::cout << "====================\n";
+  double subtotal = meal_cost;
+  std::cout << std::fixed << std::setprecision(2);
+  std::cout << "Subtotal: $" << meal_cost << "\n";
+  taxes = meal_cost * CA_TAX;
+  std::cout << "Taxes: $" << taxes << "\n";
+  tip = subtotal * tip_percentage / TIP_DIVISOR;
+  std::cout << "Tip: $" << tip << "\n";
+  std::cout << "===================="
+            << "\n";
+  total = meal_cost + tip + taxes;
+  std::cout << "Total: $" << total << "\n";
   return 0;
 }
