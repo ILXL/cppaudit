7464f8625bbcce2ab461c07a98fa259916609845
Katie Dektar <katie.dektar@gmail.com>
2020-08-25 20:25:04 -0700

    Use google style, add comment to example Makefile, delete executable

--- diff_format.sh
+++ diff_format.sh
@@ -4,8 +4,8 @@ OS_NAME=`uname -s | tr A-Z a-z`
 for file in "$@"
 do
   echo -e "\n[Checking $file]"
-  if [[ "$OS_NAME" != "darwin" ]]; then 
-    diff --suppress-common-lines --color=always -u1 <(cat $file) <(clang-format $file)
+  if [[ "$OS_NAME" != "darwin" ]]; then
+    diff --suppress-common-lines --color=always -u1 <(cat $file) <(clang-format --style=Google $file)
   else
     diff --suppress-common-lines -U1 <(cat $file) <(clang-format $file)
   fi
