f09460f7665490bf3f5919ba8e16d047839f29ca
Katie Dektar <katie.dektar@gmail.com>
2020-08-28 13:28:27 -0700

    Get it working on mac

--- Makefile
+++ Makefile
@@ -14,6 +14,11 @@ HAS_CLANGTDY  		:= $(shell command -v clang-tidy 2> /dev/null)
 HAS_CLANGFMT  		:= $(shell command -v clang-format 2> /dev/null)
 HAS_GTEST         	:= $(shell echo -e "int main() { }" >> test.cc ; clang++ test.cc -o test -lgtest 2> /dev/null; echo $$?; rm -f test.cc test;)
 
+ifeq ($(OS_NAME), darwin)
+	COMPILE_FLAGS := $(MAC_COMPILE_FLAGS)
+	UT_COMPILE_FLAGS := $(MAC_UT_COMPILE_FLAGS)
+endif
+
 ifeq ($(UTNAME),)
   UTNAME = unittest.cpp
 endif
--- examples/tools/settings/config.mk
+++ examples/tools/settings/config.mk
@@ -4,6 +4,10 @@ UTNAME		:= unittest.cc
 COMPILE_FLAGS		:= -lm -lX11 -lpthread
 # Flags added to unittest compilation step
 UT_COMPILE_FLAGS	:= -lm -lX11 -lpthread
+# Flags added for mac compilation, if different from COMPILE_FLAGS
+MAC_COMPILE_FLAGS	:= -lm -I/opt/X11/include -lpthread -lX11 -lstdc++ -I/usr/X11R6/include -L/usr/X11R6/lib
+# Flags added for mac unittest compilation step, if different from UT_COMPILE_FLAGS
+MAC_UT_COMPILE_FLAGS := -lm -lpthread -lX11 -I/usr/X11R6/include -L/usr/X11R6/lib
 # Space-separated list of header files (e.g., algebra.hpp)
 HEADERS      		:=
 # Space-separated list of implementation files (e.g., algebra.cpp)
